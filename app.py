08f9a4a3e0c9febf8d9036c8cf9ef4898f6839fd
sdelbecque13 <s.delbecque@gmail.com>
2023-11-15 15:59:13 +0100

    Deuxieme commit

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ def poser_question(question, reponse, chances_restantes = 3):
     while chances_restantes > 0:
         reponse_utilisateur = input(question)
         if reponse_utilisateur.lower() == reponse.lower():
-            print("Bonne réponse!")
+            print("Bonne réponse !")
             return chances_restantes
         else:
             chances_restantes -= 1
-            print(f"Mauvaise réponse. Il vous reste {chances_restantes} chances.")
+            print(f"Mauvaise réponse ! Il vous reste {chances_restantes} chances.")
     return chances_restantes 
 
 def executer_quiz():
